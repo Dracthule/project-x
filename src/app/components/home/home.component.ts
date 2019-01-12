455fdb6926f1fb9e2e5f8640521697b4673e0fbc
Dracthule <43589285+Dracthule@users.noreply.github.com>
2019-01-12 00:15:04 -0800

    video and avatar updates as well as button added on

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  //  array: [
+  //   {
+  //     title: 'Sexy Vannah',
+  //     subtitle: 'Vannah is LIVE!',
+  //     description:
+  //       'Vannah is one of the worlds top models with millions of fans accross the nation.',
+  //     src: 'https://static.videogular.com/assets/videos/videogular.mp4',
+  //     type: 'video/mp4'
+  //   }
+  // ];
+}
